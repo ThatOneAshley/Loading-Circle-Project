9a020d5a2ee27e7eae9bda3bfb541dc542acefbc
ThatOneAshley <130107330+ThatOneAshley@users.noreply.github.com>
2023-11-15 12:18:18 -0500

    briggsashley_CanvasFrame_Dynamic.java

--- briggsashley_CanvasFrame_Dynamic.java
+++ briggsashley_CanvasFrame_Dynamic.java
@@ -23,7 +23,7 @@ public class CanvasFrame_Dynamic
         canvas = new CanvasPanel();
         canvas.setPreferredSize(new Dimension(2 * canvas.X_CORNER + 
         canvas.CANVAS_WIDTH, 2 * canvas.Y_CORNER + canvas.CANVAS_HEIGHT));
-        frame.getContentPane().add(canvas); //put the canvas in the fram
+        frame.getContentPane().add(canvas); //put the canvas in the frame
         frame.pack();                       //make everything the preferred size
         frame.setVisible(true);             //show the frame
     }
